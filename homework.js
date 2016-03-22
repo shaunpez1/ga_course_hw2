9ec793a95fec9d2b140dd490fb90fb656a5c8855
Shaun Pezeshki <shaunpez@gmail.com>
2016-03-21 23:31:05 -0700

    Simplified the prototypes

--- homework.js
+++ homework.js
@@ -1,47 +1,44 @@
 var animalsList = ["Cat", "Dog", "Polar Bear", "Grizzly Bear", "Antelope", "Kangaroo", "Bear"];
 
+// Constructor
 function Animals (animalsListing){
   this.animals = animalsListing;
 }
 
 // Return animals that have 3 letters
 Animals.prototype.animalThreeLetters = function(){
-  var animalThree = this.animals.filter(function(animal) {
+  return this.animals.filter(function(animal) {
     return animal.length == 3;
   });
-  return animalThree;
 }
 
 // Return animals that have 2 words
 Animals.prototype.animalTwoWords = function(){
-  var animalTwo = this.animals.filter(function(animal) {
+  return this.animals.filter(function(animal) {
     var pattern = /[A-z]+ [A-z]+/i;
     return pattern.test(animal);
   });
-  return animalTwo;
 }
 
 // Return animals that don't have first letter contains k
 Animals.prototype.animalwithoutK = function(){
-  var animalwithout = this.animals.filter(function(animal) {
+  return this.animals.filter(function(animal) {
     var pattern = /^k/i;
     return !pattern.test(animal);
   });
-  return animalwithout;
 }
 
 // Return animal named Teddy Bear to bear
 Animals.prototype.teddyToBear = function(){
-  var teddyToBear = this.animals.map(function(animal) {
+  return this.animals.map(function(animal) {
     return animal.replace(/bear/i, "Teddy Bear");
   });
-  return teddyToBear;
 }
 
 // Return animal named Teddy Bear to bear
 Animals.prototype.replaceCatDog = function(){
 
-  var replaceCatDog = this.animals.map(function(animal) {
+  return this.animals.map(function(animal) {
     var pattern = /cat/i;
     var patternTwo = /dog/i;
 
@@ -52,7 +49,6 @@ Animals.prototype.replaceCatDog = function(){
     }
     return animal;
   });
-  return replaceCatDog;
 }
 
 var Animals = new Animals(animalsList);
@@ -60,5 +56,5 @@ var Animals = new Animals(animalsList);
 console.log(Animals.animalThreeLetters());
 console.log(Animals.animalTwoWords());
 console.log(Animals.animalwithoutK());
-console.log(Animals.replaceCatDog());
 console.log(Animals.teddyToBear());
+console.log(Animals.replaceCatDog());
